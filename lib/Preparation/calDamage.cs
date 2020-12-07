3e3f2f4168e951669e8f31f3830f5f1e1e8bbe00
HungMing Liang <zxjonathanzx@gmail.com>
2020-12-07 19:05:22 +0800

    fix bug and add reduce damage case

--- lib/Preparation/calDamage.cs
+++ lib/Preparation/calDamage.cs
@@ -26,46 +26,79 @@ namespace lib
                     damage=earth(enemyType,myAtk,enemyDef);
                     break;
                 defalut:
-                    damage=enemyDef-myAtk;
+                    damage=myAtk-enemyDef;
                     break;
 
 
             }
 
+            if (damage<=0){
+                return 0; // in case the enemyDef too high so damage become negative then heal enemy
+            }
 
             return damage;
 
         }
 
         private static int gold(int enemyType, int myAtk, int enemyDef){
+            
             if(enemyType==2){
                 return (int)(myAtk*2.5-enemyDef);
             }
-            return enemyDef-myAtk;
+
+            if(enemyType==3){
+                return (int)(myAtk*0.5-enemyDef);
+            }
+
+            return myAtk-enemyDef;
         }
         private static int wood(int enemyType, int myAtk, int enemyDef){
+           
             if(enemyType==5){
                 return (int)(myAtk*2.5-enemyDef);
             }
-            return enemyDef-myAtk;            
+
+            if(enemyType==4){
+                return (int)(myAtk*0.5-enemyDef);
+            }
+
+            return myAtk-enemyDef;            
         }
         private static int water(int enemyType, int myAtk, int enemyDef){
+            
             if(enemyType==4){
                 return (int)(myAtk*2.5-enemyDef);
             }
-            return enemyDef-myAtk;            
+
+            if(enemyType==2){
+                return (int)(myAtk*0.5-enemyDef);
+            }
+
+            return myAtk-enemyDef;            
         }
         private static int fire(int enemyType, int myAtk, int enemyDef){
+            
             if(enemyType==1){
                 return (int)(myAtk*2.5-enemyDef);
             }
-            return enemyDef-myAtk;            
+
+            if(enemyType==5){
+                return (int)(myAtk*0.5-enemyDef);
+            }
+
+            return myAtk-enemyDef;           
         }
         private static int earth(int enemyType, int myAtk, int enemyDef){
+            
             if(enemyType==3){
                 return (int)(myAtk*2.5-enemyDef);
             }
-            return enemyDef-myAtk;            
+
+            if(enemyType==1){
+                return (int)(myAtk*0.5-enemyDef);
+            }
+
+            return myAtk-enemyDef;            
         }
 
     }
